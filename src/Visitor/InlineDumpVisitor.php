56917afd43062ab71eef8db721981ece29b3437d
Loris Leiva <loris.leiva@gmail.com>
2018-05-15 10:47:12 +0200

    :white_check_mark: Test flatten AND OR visitor

--- src/Visitor/InlineDumpVisitor.php
+++ src/Visitor/InlineDumpVisitor.php
@@ -9,6 +9,13 @@ use Lorisleiva\LaravelSearchString\Parser\QuerySymbol;
 
 class InlineDumpVisitor implements Visitor
 {
+    protected $shortenQuery;
+
+    public function __construct($shortenQuery = false)
+    {
+        $this->shortenQuery = $shortenQuery;
+    }
+
     public function visitOr(OrSymbol $or)
     {
         return 'OR(' . collect($or->expressions)->map->accept($this)->implode(', ') . ')';
@@ -27,6 +34,12 @@ class InlineDumpVisitor implements Visitor
     public function visitQuery(QuerySymbol $query)
     {
         $value = $query->value;
+
+        if ($this->shortenQuery) {
+            $value = is_array($value) ? '[' . implode(', ', $value) . ']' : $value;
+            return $query->key . (is_bool($value) ? '' : "$query->operator $value");
+        }
+
         $value = is_bool($value) ? ($value ? 'true' : 'false') : $value;
         $value = is_array($value) ? '[' . implode(', ', $value) . ']' : $value;
         return "QUERY($query->key $query->operator $value)";
