193cb407c86ebeb606931db43600839873e1fe39
Kit Burton-Senior <mail@kitbs.com>
2020-03-02 12:36:06 +0000

    relation searchstringoptions tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -47,6 +47,11 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         return $this->getModelWithOptions(['keywords' => $keywords]);
     }
 
+    public function getModelWithRelations($relations)
+    {
+        return $this->getModelWithOptions(['relations' => $relations]);
+    }
+
     public function getSearchStringManager($model = null)
     {
         return new SearchStringManager($model ?? new DummyModel);
