1a52a81f59bdab307c446f6b716bb0ef594a3aa2
Loris Leiva <loris.leiva@gmail.com>
2020-06-28 20:02:53 +0100

    wip

--- src/Options/ColumnRule.php
+++ src/Options/ColumnRule.php
@@ -3,8 +3,11 @@
 namespace Lorisleiva\LaravelSearchString\Options;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use LogicException;
+use Lorisleiva\LaravelSearchString\Concerns\SearchString;
 use Lorisleiva\LaravelSearchString\SearchStringManager;
 
 class ColumnRule extends Rule
@@ -44,7 +47,7 @@ class ColumnRule extends Rule
         $this->map = Collection::wrap(Arr::get($rule, 'map', []));
 
         if ($this->relationship) {
-            $this->fetchRelationshipModelAndManager();
+            $this->setRelationshipModelAndManager($model);
         }
     }
 
@@ -59,9 +62,39 @@ class ColumnRule extends Rule
         return $model->hasCast($column, 'boolean');
     }
 
-    protected function fetchRelationshipModelAndManager()
+    protected function setRelationshipModelAndManager(Model $model)
     {
-        $this->column; // TODO
+        $relation = $this->getRelation($model);
+        $related = $relation->getRelated();
+        $this->relationshipModel = $related;
+
+        if (! in_array(SearchString::class, class_uses_recursive($related))) {
+            throw new LogicException(sprintf(
+                '%s must use the %s trait to be used as a relationship.', get_class($related), SearchString::class
+            ));
+        }
+
+        $this->relationshipManager = $related->getSearchStringManager();
+    }
+
+    public function getRelation(Model $model): Relation
+    {
+        $method = $this->column;
+        $relation = $model->$method();
+
+        if (! $relation instanceof Relation) {
+            if (is_null($relation)) {
+                throw new LogicException(sprintf(
+                    '%s::%s must return a relationship instance, but "null" was returned. Was the "return" keyword used?', get_class($model), $method
+                ));
+            }
+
+            throw new LogicException(sprintf(
+                '%s::%s must return a relationship instance.', get_class($model), $method
+            ));
+        }
+
+        return $relation;
     }
 
     public function __toString()
